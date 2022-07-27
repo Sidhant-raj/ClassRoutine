592f0f5e04d83e77cc35c6318b0c8bf5dcb7e550
Sidhant Raj <55508672+Sidhant-raj@users.noreply.github.com>
2022-07-27 12:51:47 +0530

    Updated Script.js #2✔️

--- Style/script.js
+++ Style/script.js
@@ -1,40 +1,39 @@
-
+// Example to use the carousel
 // $(function() {
 //     $('#carouselExampleDark').carousel(1);
-//   });   
-
+//   });
 
 // Javascript to change text according to day
-
+//
 function changeIt() {
-    
-    var date = new Date();
-    var dayOfWeek = date.getDay();
-    
-        switch(dayOfWeek) {
-            case 1:
-                $('#carouselExampleDark').carousel(0);
-            break;
-            case 2:
-                $('#carouselExampleDark').carousel(1);
-            break;
-            case 3:
-                $('#carouselExampleDark').carousel(2);
-            break;
-            case 4:
-                $('#carouselExampleDark').carousel(3);
-            break;
-            case 5:
-                $('#carouselExampleDark').carousel(4);
-            break;
-            case 6:
-                $('#carouselExampleDark').carousel(5);
-            break;
-            default:
-                $('#carouselExampleDark').carousel(0);
-      }
-    }
-    
-      window.onload = function() {
-        changeIt();
-      };
+  var date = new Date();
+  var dayOfWeek = date.getDay();
+  // var dayOfWeek = 2;
+  // console.log(dayOfWeek);
+  switch (dayOfWeek) {
+    case 1:
+      $("#carouselExampleDark").carousel(0); // Monday
+      break;
+    case 2:
+      $("#carouselExampleDark").carousel(1); // Tuesday
+      break;
+    case 3:
+      $("#carouselExampleDark").carousel(2); // Wednesday
+      break;
+    case 4:
+      $("#carouselExampleDark").carousel(3); // Thrusday
+      break;
+    case 5:
+      $("#carouselExampleDark").carousel(4); // Friday
+      break;
+    // case 6:
+    //     $('#carouselExampleDark').carousel(5); // Saturday
+    // break;
+    default:
+      $("#carouselExampleDark").carousel(0);
+  }
+}
+
+window.onload = function () {
+  changeIt();
+};
